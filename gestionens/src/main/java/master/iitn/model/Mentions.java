9a6ccf5c974796f1afb8ba0506b50bd1d39b0df9
0x_DCCXVII <elyass.hafdi@gmail.com>
2024-05-11 17:40:27 +0100

    New updates 1.0.3

--- gestionens/src/main/java/master/iitn/model/Mentions.java
+++ gestionens/src/main/java/master/iitn/model/Mentions.java
@@ -1,6 +1,5 @@
 package master.iitn.model;
 
 public enum Mentions {
-        Passable, AssezBien, Bien, TresBien, Excellent
-    }
-
+    Passable, AssezBien, Bien, TresBien, Excellent, Validé, NValidé
+}
